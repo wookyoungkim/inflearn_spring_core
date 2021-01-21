4658dfac39be465a12df7fa9cc71cad889afac9b
wookyoungkim <viliketh1s98@naver.com>
2021-01-21 20:13:48 +0900

    create : 주문 할인 도메인 테스트 개발

---
+++ src/test/java/order/OrderServiceTest.java
@@ -0,0 +1,31 @@
+package order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        //null을 넣기 위해서 Long type 사용
+        Long memberId = 1L;
+
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+
+    }
+}
